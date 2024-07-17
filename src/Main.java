8025e8b0c4fb09ce4235a5ff51175375356c0dd4
Aleksey Zelianin <azelyanin@internet.ru>
2024-07-17 21:10:19 +0300

    Упращение логики программы

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,26 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите максимальное число ->");
+        System.out.println("Введите число ->");
         Scanner sc = new Scanner(System.in);
+        int maxNumber = sc.nextInt();;
 
-        String input = sc.nextLine();
-        int arraySize;
-        boolean validNumber = false;
-
-        while (!validNumber) {
-            try {
-                arraySize = Integer.parseInt(input);
-                validNumber = true;
-                printArray(createArray(arraySize));
-
-            } catch (NumberFormatException e) {
-                System.out.println("Введённые символы не являются числом. \nПовторите ввод -> ");
-                input = sc.nextLine();
-            }
-        }
-
-    }
-
-    /**
-     * Функция для создания и заполнения массива
-     *
-     * @param arraySize - размер массива, полученного от пользователя
-     * @return - возвращает заполненный массив
-     */
-    private static int[] createArray(int arraySize) {
-        int[] array = new int[arraySize + 1]; // +1 добавлено для корректности создания массива под условия задачи.
-        for (int i = 0; i < array.length; i++) {
-            {
-                array[i] = i;
-            }
-        }
-        return array;
-    }
-
-    /**
-     * Метод вывода элементов массива.
-     *
-     * @param array созданный массив
-     */
-    private static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) {
-                System.out.println(array[i]);
-            } else if (array[i] % 3 == 0 && array[i] % 5 == 0) {
-                System.out.println("fizzbuzz");
-            } else if (array[i] % 3 == 0) {
-                System.out.println("fizz");
-            } else if (array[i] % 5 == 0) {
-                System.out.println("buzz");
+        for (int i = 0; i <= maxNumber; i++) {
+            if (i == 0) {
+                System.out.print(i + " ");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("fizzbuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("buzz ");
             } else {
-                System.out.println(array[i]);
+                System.out.print(i + " ");
             }
         }
+        sc.close();
     }
 }
+
